9ef59739c0da227e17fc83ca0ffc0a6806dfcb93
DenisIsmagilov1 <denchikism789@gmail.com>
2020-02-20 00:48:22 +0300

    Add default App component

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+
+import rootReducer from '../store/reducers';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <HashRouter>
+          <Switch>
+          </Switch>
+        </HashRouter>
+      </Provider>
+    )
+  }
+}
+
+export default App
