2f7280b6884c7454cbcc2f9c8616e13c7947a658
DenisIsmagilov1 <denchikism789@gmail.com>
2020-03-29 10:00:23 +0300

    Inintial client

--- client/public/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.header-burger').click(function(){
-    $('.header-burger, .header-menu').toggleClass('active');
-  });
-
-  $('.menu-list').click(function(){
-    $('.header-burger, .header-menu').removeClass('active');
-  })
-})
---
+++ client/src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hello Sosister!
+    </div>
+  )
+}
+
+export default App;
