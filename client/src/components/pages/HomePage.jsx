9ec60fcc3eb4d831580d8fee286e53c1a3bd6666
DenisIsmagilov1 <denchikism789@gmail.com>
2020-03-31 10:48:33 +0300

    Create Slider

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import TrendingRecipes from './../trending-recipes';
+import Slider from '../slider';
 
 const HomePage = () => {
   return (
     <div className="container">
+       <Slider>
+        <div>
+          <img src="https://cdn.lifehacker.ru/wp-content/uploads/2019/05/Depositphotos_68972477_xl-2015_1559126153-e1559129205144-630x315.jpg" alt=""/>
+        </div>
+        <div>
+          <img src="https://static.1000.menu/img/content/6685/gus-v-duhovke-s-yablokami_1498545352_9_max.jpg" alt=""/>
+        </div>
+      </Slider>
       <div className="row gutters-60">
         <TrendingRecipes/>
       </div>
---
+++ client/src/components/slider/Slider.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import SlickSlider from 'react-slick';
+
+import './Slider.css';
+
+const SliderArrow = ({className, onClick, type}) => {
+  
+  const label = type === "prev" ? <i class="fas fa-chevron-left"></i> : <i class="fas fa-chevron-right"></i>;
+
+  return <button 
+            type="button" 
+            className={className} 
+            onClick={onClick}
+          >
+            {label}
+          </button>
+}
+
+const Slider = ({children}) => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <SliderArrow type="prev"/>,
+    nextArrow: <SliderArrow/>
+  };
+
+  return (
+    <SlickSlider {...settings}>
+      {children}
+    </SlickSlider>
+  )
+}
+
+export default Slider
